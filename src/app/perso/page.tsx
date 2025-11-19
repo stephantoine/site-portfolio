be10b0062946bd0db2353a436c13bc7877dae7f1
stephantoine <stephouille1022@gmail.com>
2025-11-19 15:20:49 +0100

    update perso

--- src/app/perso/page.tsx
+++ src/app/perso/page.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 
 const Perso = () => {
     return (
-        <div className="container max-w-3xl 2xl:max-w-7xl mx-auto pt-10 pb-20 px-4 sm:px-6 lg:px-8">
+        <div className="container max-w-5xl 2xl:max-w-7xl mx-auto pt-10 pb-20 px-4 sm:px-6 lg:px-8">
             {/* 🔸 Introduction */}
             <section className="mb-16">
                 <h1 className="text-4xl font-bold mb-4 text-center">
@@ -20,7 +20,7 @@ const Perso = () => {
 
             <section className='mb-16'>
                 <div className='flex w-full gap-8 flex-col items-center'>
-                    <video controls width={800} height={450}>
+                    <video controls width={900}>
                         <source 
                             src="https://res.cloudinary.com/dzxlzl9ng/video/upload/f_auto,q_auto/coucher_soleil_msmpjm.mp4" 
                             type="video/mp4" 
@@ -28,7 +28,7 @@ const Perso = () => {
                         Votre navigateur ne supporte pas la lecture video.
                     </video>
 
-                    <video controls width={800} height={450}>
+                    <video controls width={900}>
                         <source 
                             src="https://res.cloudinary.com/dzxlzl9ng/video/upload/f_auto,q_auto/falaises_xxuteb.mp4" 
                             type="video/mp4" 
@@ -36,7 +36,7 @@ const Perso = () => {
                         Falaises
                     </video>
 
-                    <video controls width={800} height={450}>
+                    <video controls width={900}>
                         <source 
                             src="https://res.cloudinary.com/dzxlzl9ng/video/upload/f_auto,q_auto/montagne_snizjs.mp4" 
                             type="video/mp4" 
@@ -48,18 +48,18 @@ const Perso = () => {
             </section>
 
             <section>
-                <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div className="max-w-2xl mx-auto grid grid-cols-1 sm:grid-cols-2 gap-6">
                     <Link 
                         href="https://www.youtube.com/watch?v=UcGIwjoNQzY"
-                        className="flex items-center justify-center gap-3 bg-gray-100 dark:bg-gray-800 text-primary dark:text-white py-3 px-6 rounded-2xl shadow-lg transition-transform transform hover:scale-105 hover:bg-gray-200 dark:hover:bg-gray-700"
+                        className="flex items-center justify-center gap-4 bg-primary w-full md:w-auto text-white px-8 py-3 rounded-lg hover:bg-primary/80 transition-colors"
                     > 
                         <MountainSnowIcon className="w-6 h-6"/>
-                        <span className="font-medium text-lg">Vlog des Alpes</span>
+                        <span>Vlog des Alpes</span>
                     </Link>
 
                     <Link 
                         href="https://youtu.be/mpmc1JH8vNs"
-                        className="flex items-center justify-center gap-3 bg-gray-100 dark:bg-gray-800 text-primary dark:text-white py-3 px-6 rounded-2xl shadow-lg transition-transform transform hover:scale-105 hover:bg-gray-200 dark:hover:bg-gray-700"
+                        className="flex items-center justify-center gap-4 bg-gray-700 w-full md:w-auto text-white px-8 py-3 rounded-lg hover:bg-gray-600 transition-colors"
                     > 
                         <StarIcon className="w-6 h-6"/>
                         <span className="font-medium text-lg">Montage année 2025</span>
